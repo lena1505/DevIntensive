28338b63b62a714d72de4916f8a4a7b16f990275
Елена Юрасова < >
2016-06-21 21:52:32 +0700

    Initial commit

---
+++ app/src/main/java/com/sortdesign/devintensive/ui/activities/MainActivity.java
@@ -0,0 +1,15 @@
+package com.sortdesign.devintensive.ui.activities;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.sortdesign.devintensive.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
