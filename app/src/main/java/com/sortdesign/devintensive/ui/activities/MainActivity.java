71a3b1fe244bc590dc188848547f4e388c5d3a5d
lena1505 <lena-15-05@mail.ru>
2016-06-25 03:40:20 +0700

    activity cycle was done and logged, page-proofs was begun

--- app/src/main/java/com/sortdesign/devintensive/ui/activities/MainActivity.java
+++ app/src/main/java/com/sortdesign/devintensive/ui/activities/MainActivity.java
@@ -1,15 +1,73 @@
 package com.sortdesign.devintensive.ui.activities;
 
+import android.graphics.Color;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 import com.sortdesign.devintensive.R;
+import com.sortdesign.devintensive.utils.ConstantManager;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+
+private static final String TAG = ConstantManager.TAG_PREFIX+"Main activity";
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Log.d(TAG, "OnCreate");
+
+        if (savedInstanceState == null) {
+        }else{
+        }
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        Log.d(TAG, "onStart");
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        Log.d(TAG, "onResume");
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        Log.d(TAG, "onPause");
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        Log.d(TAG, "onStop");
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        Log.d(TAG, "onDestroy");
+    }
+
+    @Override
+    protected void onRestart() {
+        super.onRestart();
+        Log.d(TAG, "onRestart");
+    }
+    @Override
+    public void onClick(View v){
+
+    }
+    @Override
+    protected void onSaveInstanceState (Bundle outState) {
+        super.onSaveInstanceState(outState);
     }
 }
